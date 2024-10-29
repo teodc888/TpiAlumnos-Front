e0f310b5bc858bb36b738aee4d34c94f25383464
teodc888 <teodc4447@gmail.com>
2024-10-29 20:18:20 -0300

    agregando funcionalidad

---
+++ js/alumno.js
@@ -0,0 +1,145 @@
+function prueba(){
+    var pru = localStorage.getItem("tokenSesion");
+
+    if(pru == null){
+        window.location.href = "/index.html";
+    }
+}
+
+prueba();
+
+async function ObtenerAlumno() {
+    var alumnoLocal = localStorage.getItem("tokenSesion");
+
+    if (alumnoLocal != null) {
+        var alumnoObjeto = JSON.parse(alumnoLocal);
+
+        fetch("https://localhost:7146/v1/api/Alumno?legajo=" + alumnoObjeto.userName, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error en la respuesta: " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById('nombreUser').innerHTML = data.nombre + ' ' + data.apellido;
+        })
+        .catch(error => {
+            console.error("Error en la solicitud:", error);
+        });
+    }
+}
+
+async function ObtenerInfoAlumno() {
+    var alumnoLocal = localStorage.getItem("tokenSesion");
+
+    if (alumnoLocal != null) {
+        var alumnoObjeto = JSON.parse(alumnoLocal);
+
+        fetch("https://localhost:7146/v1/api/Alumno/info?legajo=" + alumnoObjeto.userName, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error en la respuesta: " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Seleccionamos el contenedor donde se agregarán las tarjetas
+            const contenedorTarjetas = document.getElementById("contenedorTarjetas");
+            contenedorTarjetas.innerHTML = ""; // Limpiamos el contenido anterior
+
+            // Recorremos cada objeto de data y generamos una tarjeta
+            data.forEach(alumno => {
+                const tarjeta = document.createElement("div");
+                tarjeta.className = "col-xl-3 col-md-6 mb-4";
+                tarjeta.innerHTML = `
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        ${alumno.carrera}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">${alumno.materia}</div>
+                                    <div class="text-xs mt-1">Tipo: ${alumno.tipoMateria}</div>
+                                    <div class="text-xs">Fecha Inscripción Cursado: ${alumno.fechaInscripcionCursado}</div>
+                                    <div class="text-xs">Fecha Inscripción Materia: ${alumno.fechaInscripcionMateria}</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                
+                // Agregamos la tarjeta al contenedor
+                contenedorTarjetas.appendChild(tarjeta);
+            });
+        })
+        .catch(error => {
+            console.error("Error en la solicitud:", error);
+        });
+    }
+}
+
+async function ObtenerNotasAlumno() {
+    var alumnoLocal = localStorage.getItem("tokenSesion");
+
+    if (alumnoLocal != null) {
+        var alumnoObjeto = JSON.parse(alumnoLocal);
+
+        fetch("https://localhost:7146/v1/api/Alumno/info/notas?legajo=" + alumnoObjeto.userName, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error en la respuesta: " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Seleccionamos el cuerpo de la tabla
+            const tbody = document.querySelector("#dataTable tbody");
+            tbody.innerHTML = ""; // Limpiamos el contenido previo en la tabla
+
+            // Recorremos el array de datos para agregar filas
+            data.forEach(alumno => {
+                const fila = document.createElement("tr");
+                fila.innerHTML = `
+                    <td>${alumno.legajo}</td>
+                    <td>${alumno.curso}</td>
+                    <td>${alumno.estado}</td>
+                    <td>${alumno.materia}</td>
+                    <td>${alumno.nota}</td>
+                    <td>${alumno.fechaCursado}</td>
+                `;
+                
+                // Añadimos la fila al cuerpo de la tabla
+                tbody.appendChild(fila);
+            });
+        })
+        .catch(error => {
+            console.error("Error en la solicitud:", error);
+        });
+    }
+}
+
+
+ObtenerNotasAlumno()
+
+ObtenerInfoAlumno();
+
+ObtenerAlumno();
---
+++ js/login.js
@@ -0,0 +1,63 @@
+document.getElementById("loginForm").addEventListener("submit", async function(event) {
+    event.preventDefault(); // Evita que se recargue la página
+
+    // Obtener valores de los campos de entrada
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Crear el cuerpo de la solicitud
+    const requestBody = {
+        userName: username,
+        password: password
+    };
+
+    try {
+ 
+        const response = await fetch("https://localhost:7258/api/v1/Login/Login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        // Verificar si la respuesta es exitosa
+        if (response.ok) {
+            const data = await response.json();
+            
+            // Guardar el token de sesión en localStorage si la respuesta es válida
+            if (data.response && data.response.tokenSesion) {
+                localStorage.setItem("tokenSesion", JSON.stringify(data));
+
+                if(data.rol == "Alumno"){
+                    window.location.href = "/alumno/alumno-index.html"
+                }
+                if(data.rol == "Docente"){
+                     window.location.href = "/docente/docente-index.html"
+                }
+
+            } else {
+                window.location.href = "404.html"
+            }
+        } else {
+            alert("Inicio de sesión fallido. Verifica tus credenciales.");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+          window.location.href = "404.html"
+    }
+});
+
+// Función para mostrar u ocultar la contraseña
+function togglePassword() {
+    const passwordField = document.getElementById("password");
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+}
+
+
+function logout(){
+    console.log("hola")
+    localStorage.removeItem('tokenSesion');
+    window.location.href = "/index.html"
+}
