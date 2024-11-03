45eda230c44840ed1220949e6c9c370bbe1ad1b4
teodc888 <teodc4447@gmail.com>
2024-11-03 19:13:07 -0300

    perfil para docente

--- js/docente.js
+++ js/docente.js
@@ -209,7 +209,6 @@ async function obtenerMateriasDocente() {
                     <td>${materia.materia}</td>
                     <td>${materia.carrera}</td>
                     <td>${materia.anioPlan}</td>
-                    <td><a href="modificar_materia.html?id=${materia.legajo}">Modificar</a></td>
                 `;
 
                 tbody.appendChild(fila);
@@ -453,7 +452,48 @@ async function mostrarInfo(legajoAlumno) {
     }
 }
 
+async function cargarPerfilAlumno() {
+    var alumnoLocal = localStorage.getItem("tokenSesion");
+    var alumnoObjeto = JSON.parse(alumnoLocal);
+    try {
+        const response = await fetch('https://localhost:7146/v1/api/Docente?legajo=' + alumnoObjeto.userName);
+        console.log(response)
+        if (!response.ok) {
+            
+            throw new Error("Error al obtener los datos del alumno");
+        }
+        const data = await response.json();
+        console.log(data)
+        mostrarPerfil(data);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
 
+function mostrarPerfil(alumno) {
+    const cardPerfil = document.getElementById("cardPerfil");
+    cardPerfil.innerHTML = `
+        <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 24rem;">
+            <div class="card-body">
+                <h4 class="card-title text-center text-primary">${alumno.nombre} ${alumno.apellido}</h4>
+                <h6 class="card-subtitle mb-2 text-muted text-center">Legajo: ${alumno.legajo}</h6>
+                <hr>
+                <p><strong>Documento:</strong> ${alumno.tipoDocumento} ${alumno.numeroDocumento}</p>
+                <p><strong>Fecha de Nacimiento:</strong> ${new Date(alumno.fechaNacimiento).toLocaleDateString()}</p>
+                <p><strong>Dirección:</strong> ${alumno.direccion}</p>
+                <p><strong>Localidad:</strong> ${alumno.localidad}</p>
+                <p><strong>Estado Civil:</strong> ${alumno.estadoCivil}</p>
+                <p><strong>Estado Habitacional:</strong> ${alumno.estadoHabitacional}</p>
+                <p><strong>Situación Laboral:</strong> ${alumno.situacionLaboral}</p>
+                <p><strong>Género:</strong> ${alumno.genero}</p>
+                <p><strong>Título Universitario:</strong> ${alumno.tituloUniversitario}</p>
+            </div>
+        </div>
+    `;
+}
+
+
+cargarPerfilAlumno()
 
 function logout() {
     localStorage.removeItem("tokenSesion");
